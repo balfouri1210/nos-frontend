8b7b92640b2137f39c2982cfeb462f9409dcc121
jonghoon kim <balfouri1210@gmail.com>
2019-12-15 21:41:26 +0900

    Refactor: edit axios plugin

--- client/plugins/axios.js
+++ client/plugins/axios.js
@@ -1,12 +1,22 @@
-// export default function ({ $axios, redirect }) {
+// axios interceptor 필요할 때 쓸 것. Sodagift에서 가져옴 (20191215)
+// https://axios.nuxtjs.org/extend
+
+// export default function({ $axios, store, redirect, app }) {
 //   $axios.onRequest(config => {
-//     console.log('Making request to ' + config.url);
+//     config.headers.common['Authorization'] = `Bearer ${store.state.user.auth}`;
 //   });
 
 //   $axios.onError(error => {
 //     const code = parseInt(error.response && error.response.status);
-//     if (code === 400) {
-//       redirect('/400');
+
+//     if ([401, 403].includes(code)) {
+//       app.$logout();
+
+//       return redirect(app.localePath({
+//         name: 'login'
+//       }));
 //     }
+
+//     return Promise.reject(error);
 //   });
 // }
