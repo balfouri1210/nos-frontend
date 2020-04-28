66573a389160d67c7f4cb48675a4219a7a1fe08a
jonghoon kim <balfouri1210@gmail.com>
2020-04-29 02:03:08 +0900

    Add: GA_TRACKING_ID to config

--- config/dev/env.js
+++ config/dev/env.js
@@ -2,7 +2,8 @@ const env = {
   STAGE: 'dev',
   NOS_API_URL: 'https://api.907degrees.com',
   NOS_IMAGE_URL: 'https://images.907degrees.com',
-  YOUTUBE_API_URL: 'AIzaSyAJ2FWloctY4Cf5nMatWUH8VrGn8FxVg64'
+  YOUTUBE_API_URL: 'AIzaSyAJ2FWloctY4Cf5nMatWUH8VrGn8FxVg64',
+  GA_TRACKING_ID: 'UA-164815160-1'
 };
 
 module.exports = env;
--- config/local/env.js
+++ config/local/env.js
@@ -2,7 +2,8 @@ const env = {
   STAGE: 'local',
   NOS_API_URL: 'http://localhost:3002',
   NOS_IMAGE_URL: 'https://images.907degrees.com',
-  YOUTUBE_API_URL: 'AIzaSyAJ2FWloctY4Cf5nMatWUH8VrGn8FxVg64'
+  YOUTUBE_API_URL: 'AIzaSyAJ2FWloctY4Cf5nMatWUH8VrGn8FxVg64',
+  GA_TRACKING_ID: 'UA-164815160-1'
   // NOS_API_URL: 'https://api.907degrees.com'
 };
 
