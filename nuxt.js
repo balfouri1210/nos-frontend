bcdc3f70aa91c54c329fe3f01b7fe5364d666582
jonghoon kim <balfouri1210@gmail.com>
2020-07-04 18:33:42 +0900

    Refactor: adjust npm modules and nuxt.js

--- nuxt.js
+++ nuxt.js
@@ -5,24 +5,24 @@ const app = express();
 const { Nuxt } = require('nuxt');
 const path = require('path');
 
-// app.use('/_nuxt', express.static(path.join(__dirname, '.nuxt', 'dist')));
-// const config = require('./nuxt.config.js');
-// const nuxt = new Nuxt(config);
+app.use('/_nuxt', express.static(path.join(__dirname, '.nuxt', 'dist')));
+const config = require('./nuxt.config.js');
+const nuxt = new Nuxt(config);
 
-// app.use(async (req, res, next) => {
-//   await nuxt.ready();
-//   nuxt.render(req, res, next);
-// });
+app.use(async (req, res, next) => {
+  await nuxt.ready();
+  nuxt.render(req, res, next);
+});
 
-// module.exports = app;
+module.exports = app;
 
 
 
 // 20200703
 // nuxt.js
-app.use('/_nuxt', express.static(path.join(__dirname, '.nuxt', 'dist')));
-const config = require('./nuxt.config.js');
-const nuxt = new Nuxt(config);
-app.use(nuxt.render);
+// app.use('/_nuxt', express.static(path.join(__dirname, '.nuxt', 'dist')));
+// const config = require('./nuxt.config.js');
+// const nuxt = new Nuxt(config);
+// app.use(nuxt.render);
 
-module.exports = app;
+// module.exports = app;
