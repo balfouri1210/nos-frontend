286276e7cfb680b6b242100c0235a35d41b034ba
jonghoon kim <balfouri1210@gmail.com>
2020-07-06 21:18:04 +0900

    Adjust serverless yml

--- config/serverless-env-loader.js
+++ config/serverless-env-loader.js
@@ -5,7 +5,9 @@ const serverlessEnv = {
   STAGE: stage,
   APP_NAME: env.APP_NAME,
   DOMAIN: stage === 'prod' ? env.DOMAIN : `dev.${env.DOMAIN}`,
-  REGION: env.REGION
+  REGION: env.REGION,
+  CERTIFICATE_ARN: env.CERTIFICATE_ARN,
+  HOSTED_ZONE_ID: env.HOSTED_ZONE_ID
 };
 
 module.exports = () => {
--- config/serverless-env.js
+++ config/serverless-env.js
@@ -1,7 +1,9 @@
 const env = {
   APP_NAME: 'nos-frontend',
   DOMAIN: '907degrees.com',
-  REGION: 'eu-west-2'
+  REGION: 'eu-west-2',
+  CERTIFICATE_ARN: 'arn:aws:acm:us-east-1:592854792012:certificate/048a6320-e34b-43b7-8102-dd3b69fd29ba',
+  HOSTED_ZONE_ID: 'Z2FDTNDATAQYW2'
 };
 
 module.exports = env;
