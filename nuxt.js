62539bca239b43fd7eca153d2f54d1ffd098c53c
jonghoon kim <balfouri1210@gmail.com>
2020-07-07 01:04:48 +0900

    Deploy test

--- nuxt.js
+++ nuxt.js
@@ -1,12 +1,12 @@
 // This code running on lambda
 // and return nuxt object
 const express = require('express');
-const xRequestId = require('express-request-id');
+// const xRequestId = require('express-request-id');
 const app = express();
 const { Nuxt } = require('nuxt');
 const path = require('path');
 
-app.use(xRequestId);
+// app.use(xRequestId);
 
 app.use('/_nuxt', express.static(path.join(__dirname, '.nuxt', 'dist')));
 const config = require('./nuxt.config.js');
