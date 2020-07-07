62f8a0629e215d7314c6de1aad078fe91732579a
jonghoon kim <balfouri1210@gmail.com>
2020-07-07 18:41:23 +0900

    Minor actions

--- nuxt.js
+++ nuxt.js
@@ -9,9 +9,9 @@ const path = require('path');
 // app.use(xRequestId);
 
 app.use('/_nuxt', express.static(path.join(__dirname, '.nuxt', 'dist')));
+
 const config = require('./nuxt.config.js');
 const nuxt = new Nuxt(config);
-
 app.use(async (req, res, next) => {
   await nuxt.ready();
   nuxt.render(req, res, next);
