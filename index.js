0b81b0e861cab360afc6b6e0443dcaf3d3adb359
jonghoon kim <balfouri1210@gmail.com>
2020-07-08 23:38:10 +0900

    Refactor: simplify serverless handler

--- index.js
+++ index.js
@@ -2,10 +2,6 @@ const sls = require('serverless-http');
 const binaryMimeTypes = require('./binaryMimeTypes');
 const nuxt = require('./nuxt');
 
-const handler = sls(nuxt, {
+module.exports.handler = sls(nuxt, {
   binary: binaryMimeTypes
 });
-module.exports.handler = async (event, context) => {
-  const result = await handler(event, context);
-  return result;
-};
