e3e0c24cb71b8a3216b9159cde24eb8ada938bb5
jonghoon kim <balfouri1210@gmail.com>
2020-07-09 18:22:41 +0900

    Add: serverless warmup process

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-const sls = require('serverless-http');
+const serverless = require('serverless-http');
 const binaryMimeTypes = require('./binaryMimeTypes');
 const nuxt = require('./nuxt');
 
-module.exports.handler = sls(nuxt, {
+const slsObj = serverless(nuxt, {
   binary: binaryMimeTypes
 });
+module.exports.handler = async (event, context, callback) => {
+  // Immediate response for WarmUP plugin
+  if (event.source === 'serverless-plugin-warmup') {
+    return callback(null, 'Lambda is warm!');
+  }
+
+  const result = await slsObj(event, context);
+  return result;
+};
