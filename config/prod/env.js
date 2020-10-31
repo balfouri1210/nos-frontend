35c6a27f2cb46957060e3d9d2673523f9dc8b025
jonghoon kim <balfouri1210@gmail.com>
2020-10-31 23:38:28 +0900

    Refactor: change BING api url for handling microsoft update

--- config/prod/env.js
+++ config/prod/env.js
@@ -3,9 +3,10 @@ const env = {
   NOS_URL: 'https://907degrees.com',
   NOS_API_URL: 'https://api.907degrees.com',
   NOS_IMAGE_URL: 'https://images.907degrees.com',
+  BING_API_URL: 'https://api.bing.microsoft.com/v7.0/news/search',
 
   YOUTUBE_API_KEY: 'AIzaSyDRKV44WOv8EfOf2nftTdlrSj8MLzvzd80',
-  AZURE_API_KEY: '93b5b68bdac1416c944e664aedfcecfb',
+  BING_API_KEY: '7940325feb9e48df9c06265159dce145',
   API_FOOTBALL_API_URL: 'https://api-football-v1.p.rapidapi.com/v2',
   RAPID_API_KEY: '66fadb396cmsh7e7976a97d1dac7p10ccfbjsn442c3d222ff9',
   GA_TRACKING_ID: 'UA-167249423-1',
